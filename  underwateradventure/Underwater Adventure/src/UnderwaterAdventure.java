3defe8d844d0af3fbcf31a03b418c79ac031fbec
markramasco <markramasco@5e52b094-e5c1-11de-985c-a771f7a3a295>
2009-12-11 04:32:41 +0000

    test

---
+++  underwateradventure/Underwater Adventure/src/UnderwaterAdventure.java
@@ -0,0 +1,19 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+
+public class UnderwaterAdventure extends Applet {
+	
+
+	public void init()
+	{
+	setSize(500, 500);
+	}
+	
+	public void paint (Graphics g)
+	{
+		g.drawString("Hey Guys! Ready for some underwater adventure?", 100, 250);
+	}
+}
