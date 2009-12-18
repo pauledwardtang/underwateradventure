75ec663e61dd8c6ad4deb3930ba66ab1158a1964
natbird.johanson <natbird.johanson@5e52b094-e5c1-11de-985c-a771f7a3a295>
2009-12-18 06:51:58 +0000

    I'd like to be, under the sea In an octopus' garden in the shade

---  underwateradventure/src/game/GameThread.java
+++  underwateradventure/src/game/GameThread.java
@@ -4,11 +4,15 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.Random;
 
+import objects.PhysicalObject;
 import objects.Player;
 
 public class GameThread extends Applet implements Runnable, KeyListener{
@@ -18,12 +22,31 @@ public class GameThread extends Applet implements Runnable, KeyListener{
     //the main drawing object for the back buffer
     Graphics2D graphics;
     Player player = new Player(this);
+    
+    Random randomX=new Random();
+    Random randomY=new Random();
+    
+    
+    PhysicalObject octopi_array[]={	new PhysicalObject(this),
+    								new PhysicalObject(this),
+    								new PhysicalObject(this),
+    								new PhysicalObject(this),
+    								new PhysicalObject(this),
+    								new PhysicalObject(this),
+    								new PhysicalObject(this),
+    							   };	
+   
+
     int appletSizeX = 1000;
     int appletSizeY = 400;
     AffineTransform ident = new AffineTransform();
     boolean showBounds;
+    int change;
+    
+    int currentFrameOctopi=0;
     public void init()
 	{
+    	
     	setSize(appletSizeX,appletSizeY);
     	addKeyListener(this);
     	backbuffer = new BufferedImage(appletSizeX, appletSizeY, BufferedImage.TYPE_INT_RGB);
@@ -32,6 +55,14 @@ public class GameThread extends Applet implements Runnable, KeyListener{
 		player.setX(appletSizeX/2);
 		player.setY(appletSizeY/2);
 		player.load("sub.png");
+		
+		
+		for(int i=0; i<octopi_array.length; i++){
+			octopi_array[i].setGraphics(graphics);
+			octopi_array[i].setX(randomX.nextInt(1000));
+			octopi_array[i].setY(randomY.nextInt(400));
+		}
+		
 	}
     public void update(Graphics g){
     	graphics.setTransform(ident);
@@ -41,6 +72,9 @@ public class GameThread extends Applet implements Runnable, KeyListener{
     	graphics.fillRect(0, 0, getSize().width, getSize().height);
     	player.draw();
     	player.transform();
+    	for(int i=0; i<octopi_array.length; i++){
+    		transformOctopi(octopi_array[i].getX(), octopi_array[i].getY(), i);
+    	}
     	graphics.setColor(Color.ORANGE);
     	if(showBounds)
     		player.drawBounds();
@@ -79,6 +113,31 @@ public class GameThread extends Applet implements Runnable, KeyListener{
       public void stop() {
           gameloop = null;
       }
+      
+    public void transformOctopi(double x, double y,  int i){
+    	octopi_array[i].setX(x);
+    	octopi_array[i].setY(y-1);
+	    if(currentFrameOctopi<20){
+	    	octopi_array[i].load("octopi2.png");
+	    }
+	    else{
+	    	octopi_array[i].load("octopi.png");
+	    }
+    	
+	    
+	    octopi_array[i].draw();
+	    octopi_array[i].transform();
+		currentFrameOctopi++;
+		if(currentFrameOctopi==40){
+			currentFrameOctopi=0;
+		}
+		
+		if(octopi_array[i].getY()==-50){
+			octopi_array[i].setY(450);
+		}
+			
+	
+    }
 	@Override
 	public void keyPressed(KeyEvent k) {
 		// TODO Auto-generated method stub
